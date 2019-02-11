af6b2da2b17057536b2c62f58a483daaeacb2867
Carlo Palmieri <cpalmieri@redhat.com>
2019-02-11 17:11:45 +0100

    changed from svc to service

--- gateway/src/apicast/proxy.lua
+++ gateway/src/apicast/proxy.lua
@@ -85,8 +85,8 @@ local function output_debug_headers(service, usage, credentials)
     ngx.header["X-3scale-credentials"]   = credentials
     ngx.header["X-3scale-usage"]         = usage
     ngx.header["X-3scale-hostname"]      = ngx.var.hostname
-    ngx.header["X-3scale-svc-id"]        = service.id
-    ngx.header["X-3scale-svc-name"]      = service.serializable.system_name
+    ngx.header["X-3scale-service-id"]    = service.id
+    ngx.header["X-3scale-service-name"]  = service.serializable.system_name
   end
 end
 
